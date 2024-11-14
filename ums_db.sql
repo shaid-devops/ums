f25e4887b926218751cd6ac80ba71784d12fc1bb
Saeed Ahmad <sdshaid159@gmail.com>
2024-11-14 10:37:16 +0600

    database backup

--- ums_db.sql
+++ ums_db.sql
@@ -24,8 +24,9 @@ DROP TABLE IF EXISTS `dept_names`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `dept_names` (
-  `dept_id` int(2) NOT NULL AUTO_INCREMENT,
+  `dept_id` int(9) NOT NULL AUTO_INCREMENT,
   `dept_name` char(40) NOT NULL,
+  `stu_id` int(9) NOT NULL,
   PRIMARY KEY (`dept_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -47,10 +48,11 @@ DROP TABLE IF EXISTS `lm_reg`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `lm_reg` (
-  `stu_id` int(11) NOT NULL,
+  `stu_id` int(9) NOT NULL,
   `mem_name` char(40) NOT NULL,
   `dept_name` char(20) DEFAULT NULL,
-  `contact_num` char(11) NOT NULL
+  `contact_num` char(11) NOT NULL,
+  `dept_id` int(9) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -71,12 +73,13 @@ DROP TABLE IF EXISTS `stu_reg`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `stu_reg` (
-  `stu_id` int(11) NOT NULL AUTO_INCREMENT,
+  `stu_id` int(9) NOT NULL AUTO_INCREMENT,
   `f_name` char(20) NOT NULL,
   `l_name` char(20) NOT NULL,
   `dept_name` char(30) NOT NULL,
   `email` char(40) NOT NULL,
   `passwd` varchar(64) NOT NULL,
+  `dept_id` int(9) NOT NULL,
   PRIMARY KEY (`stu_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -87,7 +90,7 @@ CREATE TABLE `stu_reg` (
 
 LOCK TABLES `stu_reg` WRITE;
 /*!40000 ALTER TABLE `stu_reg` DISABLE KEYS */;
-INSERT INTO `stu_reg` VALUES (1,'shaid','hassan','cse','shaid@mail.com','shaid'),(2,'ekramul','hossain','EEE','ekram@mail.com','ekram'),(3,'ilhan','khondakar','cse','ilhan@mail.com','ilhan'),(4,'siyam','fahim','cse','siyam@mail.com','siyam'),(5,'Tuly','akter','CSE','tuli@mail.com','tuli');
+INSERT INTO `stu_reg` VALUES (1,'shaid','hassan','cse','shaid@mail.com','shaid',0),(2,'ekramul','hossain','EEE','ekram@mail.com','ekram',0),(3,'ilhan','khondakar','cse','ilhan@mail.com','ilhan',0),(4,'siyam','fahim','cse','siyam@mail.com','siyam',0),(5,'Tuly','akter','CSE','tuli@mail.com','tuli',0);
 /*!40000 ALTER TABLE `stu_reg` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -99,7 +102,7 @@ DROP TABLE IF EXISTS `users`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `users` (
-  `u_id` int(11) NOT NULL AUTO_INCREMENT,
+  `u_id` int(9) NOT NULL AUTO_INCREMENT,
   `email` char(40) NOT NULL,
   `password` varchar(64) NOT NULL,
   PRIMARY KEY (`u_id`)
@@ -124,4 +127,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-11-14  9:09:13
+-- Dump completed on 2024-11-14 10:36:49
